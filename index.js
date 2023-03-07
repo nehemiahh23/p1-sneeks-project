33b673dc32123ae5e2b7c282f8734828f3f251cb
Yael Harosh <yaelharosh01@gmail.com>
2023-03-07 11:51:46 -0500

    added a form and review-div in the html. added the form and reviews to the detail card

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const renderSneaker = sneaker => {
     const individualCard = document.createElement('div');
     const imgCard = document.createElement('img');
     const nameCard = document.createElement('h2');
-    const detailCard = document.querySelector("#detail-div")
+    const detailCard = document.querySelector("#detail-div");
+    const reviewDiv = document.getElementById('review-div');
 
 
     imgCard.src = sneaker.image;
@@ -21,9 +22,28 @@ const renderSneaker = sneaker => {
     individualCard.addEventListener("click", () => {
         if (detailCard.style.display === "none") {
             detailCard.style.display = "block"
+            while(reviewDiv.firstChild) {
+                reviewDiv.removeChild(reviewDiv.lastChild);
+            }
+            const h3 = document.createElement('h3');
+            h3.textContent = "Reviews";
+            reviewDiv.append(h3);
+            sneaker.reviews.forEach(review => {
+                renderReview(review, reviewDiv);
+            })
         }
         else {
             detailCard.style.display = "none"
         }
+
     })
+
+    
 }
+
+function renderReview(review, reviewDiv) {
+    const p = document.createElement('p');
+    p.textContent = review;
+    reviewDiv.append(p);
+}
+
