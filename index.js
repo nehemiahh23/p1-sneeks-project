605f31819b2f995a060800691dfb177703e30f7b
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-03-07 11:52:24 -0500

    add basic details to placeholder popup

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const detailCard = document.querySelector("#detail-div")
+detailCard.style.display = "none"
+
+const detailImg = document.createElement("img")
+const detailName = document.createElement("h1")
+const detailMaker = document.createElement("p")
+// const detailPrice = document.createElement("h2")
+// const sizeDD = document.createElement("select")
+
+detailCard.append(detailImg, detailName, detailMaker)
+
 fetch('http://localhost:3000/sneakers')
     .then(resp => resp.json())
     .then(sneakersArray => sneakersArray.forEach(sneakerObj => renderSneaker(sneakerObj)))
@@ -7,13 +18,13 @@ const renderSneaker = sneaker => {
     const individualCard = document.createElement('div');
     const imgCard = document.createElement('img');
     const nameCard = document.createElement('h2');
-    const detailCard = document.querySelector("#detail-div")
-
 
     imgCard.src = sneaker.image;
     nameCard.textContent = sneaker.name;
 
     imgCard.className = 'shoe-img'
+    // line below is just here to make the display work for now
+    detailImg.className = 'shoe-img'
 
     sneakerCard.append(individualCard);
     individualCard.append(imgCard, nameCard);
@@ -21,6 +32,9 @@ const renderSneaker = sneaker => {
     individualCard.addEventListener("click", () => {
         if (detailCard.style.display === "none") {
             detailCard.style.display = "block"
+            detailImg.src = sneaker.image
+            detailName.innerText = sneaker.name
+            detailMaker.innerText = sneaker.maker
         }
         else {
             detailCard.style.display = "none"
