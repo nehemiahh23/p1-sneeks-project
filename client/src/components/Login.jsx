647686a5e18d1bd3844f36962bbdc0c1f7716e8b
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-09-24 17:30:54 -0400

    setup login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState } from 'react'
 import Signup from './Signup'
 
 function Login({ navigate }) {
+
+  const [form, setForm] = useState({
+    email: "",
+    username: "",
+    password: ""
+  })
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
+  function handleChange(e) {
+    setForm({...form, [e.target.name]: e.target.value})
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='email'>E-Mail:</label>
+        <input name='email' id='email' type='email' value={form.email} onChange={handleChange}></input><br/>
+        <label htmlFor='password'>Password:</label>
+        <input name='password' id='password' type='password' value={form.password} onChange={handleChange}></input>
+        <input type='submit' value={"Submit"}/>
+      </form>
+    </>
   )
 }
 
